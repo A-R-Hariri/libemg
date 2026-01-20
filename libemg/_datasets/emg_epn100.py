142ad66b8fafe7c39f7df99f5e10f29d7680808a
Amir Hariri <a.r.hariri2000@gmail.com>
2026-01-20 13:00:45 -0400

    Create emg_epn100.py

---
+++ libemg/_datasets/emg_epn100.py
@@ -0,0 +1,40 @@
+from libemg._datasets.dataset import Dataset
+from libemg.data_handler import OfflineDataHandler
+import pickle
+import numpy as np
+from libemg.feature_extractor import FeatureExtractor
+from libemg.utils import *
+
+class EMGEPN100(Dataset):
+    def __init__(self, dataset_file: str = 'DATASET_85', cross_user: bool = True):
+        split = '30 Reps x 12 Gestures x 43 Users (Train), 15 Reps x 12 Gestures x 42 Users (Test) --> Cross User Split'
+        if not cross_user:
+            split = '15 Reps x 12 Gestures (Train), 15 Reps x 12 Gestures (Test) from the 43 Test Users --> User Dependent Split'
+
+        Dataset.__init__(self, 
+                         smapling=(200, 500), 
+                         num_channels=(8, 8), 
+                         recording_device=('Myo', 'gForce'), 
+                         num_subjects=85, 
+                         gestures= {  
+                            "relax": 0,       # Matches EPN-612 static classes IDs
+                            "fist": 1,
+                            "wave in": 2,
+                            "wave out": 3,
+                            "open": 4,
+                            "pinch": 5,
+                            "up": 6,
+                            "down": 7,
+                            "left": 8,
+                            "right": 9,
+                            "forward": 10,
+                            "backward": 11,}, 
+                         num_reps=split,
+                         description="EMG dataset for 12 different hand gesture categories using the Myo armband and the G-force armband.", 
+                         citation="https://doi.org/10.3390/s22249613")
+        self.resolution_bit = (8, 12)
+
+        self.dataset_name = dataset_file
+
+
+        
